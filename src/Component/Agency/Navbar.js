441737eaa037cf7ca396ec9d9cc9bc4702fdce9b
Kamthegabru <154924574+Kamthegabru@users.noreply.github.com>
2025-12-04 22:31:28 +0200

    Enhance logout functionality in Navbardsa
    Added session data cleanup on logout.

--- src/Component/Agency/Navbar.js
+++ src/Component/Agency/Navbar.js
@@ -43,7 +43,10 @@ function Navbar({ isMobileOpen, setIsMobileOpen }) {
 
   const handleItemClick = (index) => {
     if (index === 4) {
+      // Clear all session data on logout
       Cookies.remove("token");
+      Cookies.remove("userRole");
+      localStorage.removeItem("userInfo");
       navigate("/");
     } else {
       setCurrentActiveButton(index);
