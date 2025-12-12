c2840779e8ea5b47fc90d92381a697c79782a7aa
Kamthegabru <154924574+Kamthegabru@users.noreply.github.com>
2025-12-12 21:48:54 +0200

    Wrap ShowRandom component with RandomState provider

--- src/Component/Agency/Random/Random.js
+++ src/Component/Agency/Random/Random.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ShowRandom from './ShowRandom';
+import RandomState from '../../../Context/Agency/Random/RandomState';
 
 function Random() {
     return (
-        <>
+        <RandomState>
             <ShowRandom />
-        </>
+        </RandomState>
     );
 }
 
